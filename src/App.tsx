e86cbbf860b4ba9d52dfcb51f761dc36769cd1fa
Morenodev085 <moreno.ferreira11@gmail.com>
2026-01-08 18:43:41 -0300

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { GlobalStyles, theme } from "./styled/globalStyled"
 function App() {
 
   return (
-    <ThemeProvider theme={{theme}}>
+    // <ThemeProvider theme={{theme}}>
+    <>
     <GlobalStyles/>
       <h1>Meu projeto esta funcionando!
       </h1>
-    </ThemeProvider>
+    </>
+    // </ThemeProvider>
   )
 }
 
